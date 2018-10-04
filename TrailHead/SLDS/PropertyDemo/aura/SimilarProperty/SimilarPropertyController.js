f5987c7238f5b46b7a121685abbfca45c0957641
Ravi Raj <rraj@salesforce.com>
2018-10-04 11:50:41 +0100

    Added component to edit ion situ

--- TrailHead/SLDS/PropertyDemo/aura/SimilarProperty/SimilarPropertyController.js
+++ TrailHead/SLDS/PropertyDemo/aura/SimilarProperty/SimilarPropertyController.js
@@ -5,6 +5,7 @@
  * 	HISTORY		: 	ID 	 	  DATE			 AUTHOR			 DESCRIPTION
  *				  	====	========		=========		=============	
  *				   	1		04/10/18		Ravi Raj		Added [navToRecord] method to navigate to similar property
+ *				   	2		04/10/18		Ravi Raj		Added [handleSuccess] & [handleCancel] methods
  **/ 
 ({
     navToRecord : function (component, event, helper) 
@@ -18,5 +19,22 @@
         var editRecordEvent = $A.get("e.force:editRecord");
         editRecordEvent.setParams({"recordId": component.get("v.property.Id")});
         editRecordEvent.fire();
+    },
+    handleSuccess : function(component, event, helper) 
+    {
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+                                "title": "Success!",
+                                "message": "The property's info has been updated.",
+                                "type": "success"
+                            });
+        toastEvent.fire();
+        helper.showHide(component);
+    },
+    handleCancel : function(component, event, helper) 
+    {
+        helper.showHide(component);
+        event.preventDefault();
     }
+
 })
