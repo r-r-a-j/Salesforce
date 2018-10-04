ea6780658b434ec8d91156a8b03a7ab83ad87474
Ravi Raj <rraj@salesforce.com>
2018-10-04 11:44:42 +0100

    Added a button to edit properties

--- TrailHead/SLDS/PropertyDemo/aura/SimilarProperty/SimilarPropertyController.js
+++ TrailHead/SLDS/PropertyDemo/aura/SimilarProperty/SimilarPropertyController.js
@@ -1,32 +1,22 @@
+/*
+ *	DESCRIP		:	Controller class
+ *  AUTHOR		: 	Ravi Raj [RR]
+ *	DATE		: 	04/10/18
+ * 	HISTORY		: 	ID 	 	  DATE			 AUTHOR			 DESCRIPTION
+ *				  	====	========		=========		=============	
+ *				   	1		04/10/18		Ravi Raj		Added [navToRecord] method to navigate to similar property
+ **/ 
 ({
-    navToRecord : function (component, event, helper) {
+    navToRecord : function (component, event, helper) 
+    {
         var navEvt = $A.get("e.force:navigateToSObject");
-        navEvt.setParams({
-            "recordId": component.get("v.property.Id")
-        });
+        navEvt.setParams({"recordId": component.get("v.property.Id")});
         navEvt.fire();
     },
     editRecord : function(component, event, helper) 
     {
-    	helper.showHide(component);
-	},
-    handleSuccess : function(component, event, helper)
-    {
-        var toastEvent = $A.get("e.force:showToast");
-        toastEvent.setParams({
-            "title": "Success!",
-            "message": "The property's info has been updated.",
-            "type": "success"
-        });
-        toastEvent.fire();
-        var recUpdate = $A.get("e.c:recordUpdated");
-		recUpdate.fire();
-        helper.showHide(component);
-    },
-    handleCancel : function(component, event, helper) 
-    {
-        helper.showHide(component);
-        event.preventDefault();
+        var editRecordEvent = $A.get("e.force:editRecord");
+        editRecordEvent.setParams({"recordId": component.get("v.property.Id")});
+        editRecordEvent.fire();
     }
-
 })
